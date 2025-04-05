c479b23603d1efb40ba234ad758ffd74cf8dab2e
forfunig06 <srisaranp123@gmail.com>
2025-04-05 22:01:59 +0530

    added content caster

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import Navbar from './elements/navbar'
 import Top from './elements/top'
+import { Content } from './elements/content';
 function Home(){
     return(
       <div> 
       <Navbar />
       <Top />
+      <Content />
       </div>
     );
 }
---
+++ src/pages/elements/content.js
@@ -0,0 +1,43 @@
+export function Content(){
+    return(
+        <div class="content">
+    <div class="product">
+        <div class="product-pic"></div>
+        <p class="product-description"></p>
+    </div>
+    <div class="product">
+        <div class="product-pic"></div>
+        <p class="product-description"></p>
+    </div>
+    <div class="product">
+        <div class="product-pic"></div>
+        <p class="product-description"></p>
+    </div>
+    <div class="product">
+        <div class="product-pic"></div>
+        <p class="product-description"></p>
+    </div>
+    <div class="product">
+        <div class="product-pic"></div>
+        <p class="product-description"></p>
+    </div>
+    <div class="product">
+        <div class="product-pic"></div>
+        <p class="product-description"></p>
+    </div>
+    <div class="product">
+        <div class="product-pic"></div>
+        <p class="product-description"></p>
+    </div>
+    <div class="product">
+        <div class="product-pic"></div>
+        <p class="product-description"></p>
+    </div>
+    <div class="product">
+        <div class="product-pic"></div>
+        <p class="product-description"></p>
+    </div>
+    <div class="cnt-ldr"></div>
+</div>
+    );
+}
